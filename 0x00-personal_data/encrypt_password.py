bee92f640106400e1424476cb59452d230a6dee2
Radouane Baba <radoine.baba04@gmail.com>
2024-05-19 16:08:21 +0100

    final

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,4 +4,11 @@ import bcrypt
 
 def hash_password(password: str) -> bytes:
     """hash a password using hashpw"""
-    return bcrypt.hashpw(password, bcrypt.gensalt())
+    byte_pass = bytes(password, "utf-8")
+    return bcrypt.hashpw(byte_pass, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """is valid hash password"""
+    byte_pass = bytes(password, "utf-8")
+    return bcrypt.checkpw(byte_pass, hashed_password)
