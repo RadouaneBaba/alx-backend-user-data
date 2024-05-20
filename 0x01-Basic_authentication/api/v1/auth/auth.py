fa7c9863367f668ec38ebe2c9de4c8cac403b3fd
Radouane Baba <radoine.baba04@gmail.com>
2024-05-20 23:49:23 +0100

    authclass

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+""" Doc Doc Doc """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    Auth class implementation
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """reuire auth doc"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Authorization header"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """Current user implementation"""
+        return None
