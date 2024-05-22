e931d19200a372d740edec98a63287cc1040f6db
Radouane Baba <radoine.baba04@gmail.com>
2024-05-23 00:31:33 +0100

    test

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,9 +63,8 @@ def before_requet():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    if auth.authorization_header(request) and auth.session_cookie(request):
+    if auth.session_cookie(request) is None:
         abort(401)
-        return None
     request.current_user = auth.current_user(request)
 
 
