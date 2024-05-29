314602d079c9cda2d4604085d285f3239c1dbd06
Radouane Baba <radoine.baba04@gmail.com>
2024-05-29 10:03:16 +0100

    test

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,16 +26,18 @@ class Auth:
         except ValueError:
             raise ValueError(f"User {email} already exists")
         except Exception:
-            user = self._db.add_user(email, str(_hash_password(password)))
+            user = self._db.add_user(
+                email, _hash_password(password).decode("utf-8")
+            )
             return user
 
     def valid_login(self, email: str, password: str) -> bool:
         """valid login method"""
         try:
             user = self._db.find_user_by(email=email)
-            if bcrypt.checkpw(
-                user.hashed_password.encode("utf-8"), _hash_password(password)
-            ):
+            hashed_pw = user.hashed_password.encode("utf-8")
+            byte_pw = password.encode("utf-8")
+            if bcrypt.checkpw(byte_pw, hashed_pw):
                 return True
             return False
         except Exception:
