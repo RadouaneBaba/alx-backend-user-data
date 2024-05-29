542d4389c85632d15dc6e7b5839678207edc02d1
Radouane Baba <radoine.baba04@gmail.com>
2024-05-29 21:22:36 +0100

    finish

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,4 +1,4 @@
-#!/user/bin/env python3
+#!/usr/bin/env python3
 import requests
 
 TEST_URL = "http://localhost:5000"
@@ -82,7 +82,9 @@ EMAIL = "guillaume@holberton.io"
 PASSWD = "b4l0u"
 NEW_PASSWD = "t4rt1fl3tt3"
 
+
 if __name__ == "__main__":
+
     register_user(EMAIL, PASSWD)
     log_in_wrong_password(EMAIL, NEW_PASSWD)
     profile_unlogged()
