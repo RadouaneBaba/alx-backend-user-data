6a923314fbd2f943f6971db07cc3bdded0522377
Radouane Baba <radoine.baba04@gmail.com>
2024-05-29 20:49:23 +0100

    test

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,5 +49,14 @@ def logout():
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile():
+    """profile functionality"""
+    user = AUTH.get_user_from_session_id(request.cookies.get("session_id"))
+    if not user:
+        abort(403)
+    return jsonify({"email": user.email})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -74,3 +74,11 @@ class Auth:
         """destroy session by user id"""
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """get reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, reset_token=_generate_uuid())
+        except Exception:
+            raise ValueError
