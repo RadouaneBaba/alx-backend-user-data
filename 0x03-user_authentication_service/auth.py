16c0948818dd02595dc42a12c68d2d3f1d7d52b0
Radouane Baba <radoine.baba04@gmail.com>
2024-05-29 21:06:35 +0100

    fix

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,7 +88,7 @@ class Auth:
     def update_password(self, reset_token: str, password: str) -> None:
         """update password functionality"""
         try:
-            user = self._db.find_user_by(reset_token)
+            user = self._db.find_user_by(reset_token=reset_token)
             self._db.update_user(
                 user.id,
                 hashed_password=_hash_password(password).decode("utf8"),
